37379291bae860027957ec2733eeb07f28681c7c
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-03 01:29:17 -0800

    Magellan foundation sticky component added.

--- app/views/foundation-view.js
+++ app/views/foundation-view.js
@@ -1,10 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-	didInsertElement: function() {
-		Ember.$(document).ready(function() {
-			Ember.$(document).foundation();
+  didInsertElement: function() {
+    Ember.$(document).ready(function() {
+        Ember.$(document).foundation ({
+          "magellan-expedition": {
+            active_class: 'active', // specify the class used for active sections
+            threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+            destination_threshold: 20, // pixels from the top of destination for it to be considered active
+            throttle_delay: 50, // calculation throttling to increase framerate
+            fixed_top: 0, // top distance in pixels assigend to the fixed element on scroll
+            offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
+          }
+        });
+      });
+    }
+  });
+
       // Ember.$( "li" ).add( "p" ).css( "background-color", "red" );
-		});
-	}
-});

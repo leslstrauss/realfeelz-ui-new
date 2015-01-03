1796f7cbea68eb6f33b7d43198a8b5ea63beb0f2
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-03 14:30:13 -0800

    Using Stick js instead of Magellan and added z-indexes to css.

--- Brocfile.js
+++ Brocfile.js
@@ -20,23 +20,23 @@ var app = new EmberApp();
 // app.import('vendor/captcha/captcha.js');
 app.import('bower_components/recaptcha-ajax/recaptcha_ajax.js');
 
-app.import('bower_components/foundation/js/foundation.js');
-app.import('bower_components/foundation/css/foundation.css');
-app.import('bower_components/foundation/js/vendor/modernizr.js');
-app.import('bower_components/foundation/js/vendor/fastclick.js');
-app.import('bower_components/foundation/js/vendor/jquery.cookie.js');
-app.import('bower_components/foundation/js/vendor/placeholder.js');
+// app.import('bower_components/foundation/js/foundation.js');
+// app.import('bower_components/foundation/css/foundation.css');
+// app.import('bower_components/foundation/js/vendor/modernizr.js');
+// app.import('bower_components/foundation/js/vendor/fastclick.js');
+// app.import('bower_components/foundation/js/vendor/jquery.cookie.js');
+// app.import('bower_components/foundation/js/vendor/placeholder.js');
 
 // app.import('bower_components/fullpage.js/jquery.fullPage.js');
 // fullpage css incompatible with fullbackground page scrolling functionality
 // app.import('bower_components/fullpage.js/jquery.fullPage.css');
 
-app.import('bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js');
+// app.import('bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js');
 // app.import('bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js');
-app.import('bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css');
+// app.import('bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css');
 // app.import('bower_components/jquery-mousewheel/jquery.mousewheel.js');
 //
-// app.import('bower_components/jquery-sticky/jquery.sticky.js');
+app.import('bower_components/jquery-sticky/jquery.sticky.js');
 
 // app.import('vendor/jquery-easy-ticker-master/jquery.easy-ticker.js');
 // app.import('vendor/jquery-easy-ticker-master/test/jquery.easing.min.js');
--- app/views/sticky-view.js
+++ app/views/sticky-view.js
@@ -4,10 +4,10 @@ export default Ember.View.extend({
   didInsertElement: function() {
     Ember.$(document).ready(function() {
       Ember.$(window).load(function() {
-        Ember.$("#sticker").sticky({
-          topSpacing: 0,
-          center: true,
-          className: "hey"
+        Ember.$("#header").sticky({
+            topSpacing: 0,
+            bottomSpacing: 0,
+            getWidthFrom: '.row',
         });
       });
     });
